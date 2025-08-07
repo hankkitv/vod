29964fef3977e8c945171986ff69b0d9ccb7c512
hankkitv <154308499+hankkitv@users.noreply.github.com>
2025-08-07 21:01:06 +0900

    Create episode1.js

---
+++ plugins/episode1.js
@@ -0,0 +1,8 @@
+// plugins/episode1.js
+export default {
+  title: "Ep. 1 – QooQoo Boramae Park Branch",
+  description: "In this episode Umman Lee visits QooQoo, one of her favorite all-you-can-eat restaurants in Korea. She is working as a model/actress.",
+  duration: 855,
+  trailerUrl: "https://www.youtube.com/embed/AyOEkkx1lbA",
+  category: "Korean Food Tour"
+};
