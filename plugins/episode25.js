1df428b527fbf8961aedf32c068ded65609e6942
hankkitv <154308499+hankkitv@users.noreply.github.com>
2025-08-08 21:30:34 +0900

    Create episode25.js

---
+++ plugins/episode25.js
@@ -0,0 +1,8 @@
+// plugins/episode25.js
+export default {
+  title: "Ep. 25 – Les hommes viennent de Mars, les femmes viennent de Vénus",
+  description: "In this episode, Tom & Soraya visit Watsso, unlimited beef restaurant located in Seogyo-dong, Mapo-gu, Seoul, Korea.",
+  duration: 3457,
+  trailerUrl: "https://www.youtube.com/embed/Yn9tz9INhjg",
+  category: "Korean Food Tour"
+};
