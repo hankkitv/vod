48d6e8a10f2a95a6455544b3dc57e77d6bab1360
hankkitv <154308499+hankkitv@users.noreply.github.com>
2025-08-08 21:22:44 +0900

    Create episode9.js

---
+++ plugins/episode9.js
@@ -0,0 +1,8 @@
+// plugins/episode9.js
+export default {
+  title: "Ep. 9 – An EPIK teacher who knows a thing or two about gopchang! ",
+  description: "In this episode, Yemisi visited Kimdeokhu's Gopchangjo hongdae main branch and tried beef intestine.  She is working as an EPIK teacher in Korea.",
+  duration: 2166,
+  trailerUrl: "https://www.youtube.com/embed/wlnvg2RHERs",
+  category: "Korean Food Tour"
+};
