324ee378a6d99691d2acd44db4b89f89866f1ebc
hankkitv <154308499+hankkitv@users.noreply.github.com>
2025-08-08 22:04:11 +0900

    Create episode31.js

---
+++ plugins/episode31.js
@@ -0,0 +1,8 @@
+// plugins/episode31.js
+export default {
+  title: "Ep. 31 – Lamb for Christmas",
+  description: "In this episode, Duncan & Candi visit Rajang Yang galbi Yang Kkochi Muhan-refill, unlimited skewer restaurant located in Yeoksam-dong, Gangnam-gu, Seoul, Korea.",
+  duration: 3412,
+  trailerUrl: "https://www.youtube.com/embed/uO8jsZYIkTI",
+  category: "Korean Food Tour"
+};
