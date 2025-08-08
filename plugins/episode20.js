f97e77233b0a5a4cb86fd6387ae0b3b0b9f297f8
hankkitv <154308499+hankkitv@users.noreply.github.com>
2025-08-08 21:26:32 +0900

    Create episode20.js

---
+++ plugins/episode20.js
@@ -0,0 +1,8 @@
+// plugins/episode20.js
+export default {
+  title: "Ep. 20 – Christmas in April",
+  description: "In this episode, Alicia & Maxime visit Zzunhou, unlimited lamb ribs & skewers restaurant located in Sillim-dong, Gwanak-gu, Seoul, Korea.",
+  duration: 2646,
+  trailerUrl: "https://www.youtube.com/embed/HMnZC7jFYO8",
+  category: "Korean Food Tour"
+};
